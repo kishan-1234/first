a234ac3326492158d1afeb9aa74b10a0ffe656a8
kishan-1234 <kishan.ngm@gmail.com>
2018-11-16 11:02:06 +0530

    updated with bt generation

--- tools.py
+++ tools.py
@@ -12,9 +12,7 @@ import pexpect
 import requests
 import datetime as dt
 from colorama import Fore,init,Style
-init(autoreset=True)
-
-print(Fore.RED+'Hello {Fore.GREEN}Everyon!{Style.RESET_ALL}')
+#print(Fore.RED+'Hello {Fore.GREEN}Everyon!{Style.RESET_ALL}')
 
 LOG_FILENAME=__file__
 LOG_FILENAME=LOG_FILENAME.split('.')[0]+'.log'
@@ -134,96 +132,100 @@ def ns_ha_check(session,session_ip,ha_ip):
             else: 	
             	return 0	
 
-def ns_error_check(session,session_ip):
 
-    if not session:
-        logger.info("Cannot ssh into device "+session_ip)
-    	return 1
-    else:
-	cmd=exec_cmd(session,"sh version")
+def fun(session,folder,crash_dict,crash_file):
+
+	cmd = exec_cmd(session,"cd /var/core/"+folder)
+	cmd = exec_cmd(session,"gunzip "+crash_file)
+	cmd = exec_cmd(session,"what "+crash_file.split('.')[0])
+	logger.info(cmd)
 	if re.search(r'(?<=NS)\d+\.\d',cmd):
 		release = re.search(r'(?<=NS)(\d+\.\d)',cmd)
 		release = release.group(1)
-        if re.search(r'(?<=Build\s).*?\.nc',cmd):
-		build = re.search(r'(?<=Build\s)(.*?)\.nc',cmd)
+        if re.search(r'(?<=Build\s).*?(\.nc|,)',cmd):
+		build = re.search(r'(?<=Build\s)(.*?)(\.nc|,)',cmd)
 		build = build.group(1)
-	if re.search(r'\(\d+-\w+\)',cmd):
-		bit = re.search(r'\((\d+)-\w+\)',cmd)
-		bit = bit.group(1)
+	cmd = exec_cmd(session,"file "+crash_file.split('.')[0])
+	logger.info(cmd)
+	bit_flag = 0
+	if re.search(r'64-bit',cmd):
+		bit_flag = 1
 	build_name_unmod=release+"-"+build+"_nc"
-	release_dict = {'10.5':'tagma','11.0':'ion','11.1':'kopis','12.0':'oban','12.1':'kamet'}
+	release_dict = {'10.5':'tagma','11.0':'ion','11.1':'kopis','12.0':'oban','12.1':'kamet','13.0':'mana'}
 	release=release_dict[release]
 	build=re.sub(r'\.','_',build)
-	if release=='oban' or release=='kamet':
-		build_name=release+'_'+build+'_nc_'+bit
+	if release=='oban' or release=='kamet' or release == 'mana':
+		if bit_flag == 1:
+			build_name=release+'_'+build+'_nc_64'
+		else:
+			build_name=release+'_'+build+'_nc_32'
 	else:
 		build_name=release+'_'+build+'_nc'
-	logger.info("Build present on the box is "+build_name)	
-        session['prompt']='#'
-        cmd=exec_cmd(session,"shell")
-        cmd=exec_cmd(session,"cd /var/core")
-        logger.info(cmd)
-	cmd=exec_cmd(session,"date")
-	cmd = cmd.split()
-	month = cmd[2]
-	date = int(cmd[3])
-	logger.info("Date set in Netcaler is "+str(date)+" & month is "+month)
-	cmd = exec_cmd(session,"ls -lrth")
-	cmd = cmd.split()
-	del cmd[0:4]
-	del cmd[-1]
-        n = len(cmd)/9
-	files = list()
-	for i in xrange(n):
-		if cmd[9*i+5]==month and (int(cmd[9*i+6])-2) <= date <= int(cmd[9*i+6]):
-			if re.match(r'^\d+$',cmd[9*i+8]): 
-				files.append(cmd[9*i+8]) 
-#        cmd=exec_cmd(session,"cat bounds")
-#        logger.info(cmd)
-#        cmd=cmd.split()
-#        n=cmd[2]
-#        cmd=exec_cmd(session,"cd "+n)
-#        logger.info(cmd)
-#        cmd=exec_cmd(session,"ls")
-#        logger.info(cmd)
-#	cmd=exec_cmd(session,"cd .."
-	cmd=exec_cmd(session,"ls "+"dbgbins-"+build_name_unmod)
-	logger.info(cmd)
+	dbg_file_tgz = 'dbgbins-'+build_name_unmod+'.tgz'
+	dbg_file = 'dbgbins-'+build_name_unmod
+	logger.info("Crash file is of build "+build_name)
+	logger.info("dbgbins file to be copied is "+dbg_file_tgz)
+	cmd = exec_cmd(session,'ls /var/core/'+dbg_file)	
 	if re.search(r'No such',cmd):
-		cmd=exec_cmd(session,"cp /var/nsinstall/"+build_name+"/dbg*.tgz .")
-		logger.info(cmd)
-		cmd=exec_cmd(session,"tar -zxvf dbg*.tgz")
-		logger.info(cmd)
-	for n in files:
-		cmd=exec_cmd(session,"cd "+n)
-        	logger.info(cmd)
-        	cmd=exec_cmd(session,"ls")
-		logger.info(cmd)
-		core_file_list=cmd.split()
-		del core_file_list[0]
-		del core_file_list[-1]
-		cmd=exec_cmd(session,"cd ..")
-		for i in range(len(core_file_list)):	
-			if re.search(r'\.gz',core_file_list[i]):
-				cmd=exec_cmd(session,"gunzip "+n+"/"+core_file_list[i])	
-				logger.info(cmd)
-			cmd=exec_cmd(session,"file "+n+"/"+core_file_list[i])
-			logger.info(cmd)		
-        session['prompt']='>'
-        cmd=exec_cmd(session,"exit")
-        return 0
+		cmd = exec_cmd(session,"tar -zxvf /var/nsinstall/"+build_name+"/"+dbg_file_tgz+" -C /var/core/.")
+		if re.search(r'Error opening',cmd):
+			logger.info("Build folder is not present inside /var/nsinstall")
+			return
+	else:
+		logger.info("dbgbins already present in /var/core")		
+	session['prompt'] = '\(gdb\)'
+	if bit_flag == 1:
+		cmd = exec_cmd(session,'gdb /var/core/'+dbg_file+'/amd64/'+crash_file.split('-')[0].lower())
+	else:
+		cmd = exec_cmd(session,'gdb /var/core/'+dbg_file+'/i386/'+crash_file.split('-')[0].lower())
+	cmd = exec_cmd(session,'set pagination off')
+	cmd = exec_cmd(session,'set solib-absolute-prefix /dev/null')	
+	cmd = exec_cmd(session,'nosharedlibrary')
+	cmd = exec_cmd(session,'set solib-search-path '+dbg_file+'/i386')
+	cmd = exec_cmd(session,'core-file '+crash_file.split('.')[0])	
+	cmd = exec_cmd(session,'bt')
+	logger.info(cmd)
+	session['prompt'] = '#'
+	cmd = exec_cmd(session,'q')
+	cmd = exec_cmd(session,'gzip '+crash_file.split('.')[0])
+	logger.info(cmd)	
+
+def ns_error_check(session,session_ip,crash_dict):
+
+    if not session:
+        logger.info("Cannot ssh into device "+session_ip)
+    else:
+	bit_flag = 0  # 64 bit flag
+	cmd=exec_cmd(session,"set cli mode -page OFF -color OFF -timeout 36000")	
+	session['prompt'] = '#'
+	cmd = exec_cmd(session,"shell")
+	for i in crash_dict:
+		k = 0
+		if crash_dict[i][0].split('-')[0] == 'nscac64p':
+			logger.info(crash_dict[i][0]+" crash is not handled")
+		else:
+			fun(session,i,crash_dict,crash_dict[i][0])
+		for j in range(1,len(crash_dict[i])):
+			if crash_dict[i][j].split('-')[0] == 'NSPPE':
+				if (int(crash_dict[i][k].split('-')[1])+1) == int(crash_dict[i][j].split('-')[1]):
+					k = j
+					continue
+				else:
+					fun(session,i,crash_dict,crash_dict[i][j])
+			else:
+				logger.info("Crash not handled for "+crash_dict[i][j]+" type of crash") 
 
 def usage():
     
-    logger.info("Usage : "+__file__.split('.')[0]+".py <Resource file> [-ha] [-c] [-ap]")
-    logger.info("\t\t-ha To check if NSIP is present and HA pair is created, if not present then script will create HA")
-    logger.info("\t\t-c To check for errors in /var/core bounds on NetScaler")
-    logger.info("\t\t-ap To make Admin partition on testbed 7700, Pleae remember to provide ip for the testbed in a file")
+	logger.info("Usage : "+__file__.split('.')[0]+".py <Resource file> [-ha] [-c] [-ap]")
+    	logger.info("\t\t-ha To check if NSIP is present and HA pair is created, if not present then script will create HA")
+    	logger.info("\t\t-c To check for errors in /var/core bounds on NetScaler")
+    	logger.info("\t\t-ap To make Admin partition on testbed 7700, Pleae remember to provide ip for the testbed in a file")
 
 def main():
 	
    # try:
-	logger.info(Fore.GREEN+'BYE!!!')
+	#logger.info(Fore.GREEN+'BYE!!!')
         if len(sys.argv)<=2:
             logger.info("Please provide valid arguements\nSee usage")
             usage()
@@ -263,21 +265,36 @@ def main():
          		test_start_time = dt.datetime(int(start_time.group(1)),int(start_time.group(2)),int(start_time.group(3)),int(start_time.group(4)),int(start_time.group(5)),int(start_time.group(6)))
          		testinstance_time = test_start_time
 			logger.info(testinstance_time)
-			for i in file_list:
-				dir_details = i.split()
-               	 		dir_name = dir_details[6]
-                 		if dir_name == "bounds":
+			crash_dict = dict()
+			for i in file_list: #all folder inside /var/core
+				folder_details = i.split()
+               	 		folder_name = folder_details[6]
+                 		if folder_name == "bounds":
                          		continue
-                 		time_match = re.match("(\d+)-(\d+)-(\d+)-(\d+):(\d+):(\d+)",dir_details[5])
+                 		time_match = re.match("(\d+)-(\d+)-(\d+)-(\d+):(\d+):(\d+)",folder_details[5])
                          	time = dt.datetime(int(time_match.group(1)),int(time_match.group(2)),int(time_match.group(3)),int(time_match.group(4)),int(time_match.group(5)),int(time_match.group(6)))
-                 		if (testinstance_time - time < delta) and (re.match("^(\d+)",dir_name)):		
-					logger.info("Crash might be seen in folder "+dir_name)
-					cmd = exec_cmd(ip1_session,'cd /var/core/'+dir_name)
+                 		if (testinstance_time - time < delta) and (re.match("^(\d+)",folder_name)):	#confirm if folder is having right crash	
+					logger.info("Crash might be seen in folder "+folder_name)
+					crash_dict[folder_name] = list()
+					cmd = exec_cmd(ip1_session,'cd /var/core/'+folder_name)
 					cmd=exec_cmd(ip1_session,"ls -lD \"%F-%T\"")
          				file_list = cmd.split('\n')
          				del file_list[0:2]
          				del file_list[len(file_list)-1]		
-					logger.info(file_list)
+					for j in file_list: #all files inside /var/core/<folder>
+						dir_details = j.split()
+						dir_name = dir_details[6]
+						time_match = re.match("(\d+)-(\d+)-(\d+)-(\d+):(\d+):(\d+)",dir_details[5])
+						time = dt.datetime(int(time_match.group(1)),int(time_match.group(2)),int(time_match.group(3)),int(time_match.group(4)),int(time_match.group(5)),int(time_match.group(6)))
+						if (testinstance_time - time < delta) and (re.match(r'^n',dir_name,re.IGNORECASE)): #confirm if the file is actually a crash
+							logger.info(dir_name+" crash file is seen")
+							crash_dict[folder_name].append(dir_name)
+			ip1_session['prompt'] = '>'
+			cmd = exec_cmd(ip1_session,'exit')
+			crash_dict = dict( [(k,v) for k,v in crash_dict.items() if len(v)>0])
+			logger.info(crash_dict)
+			ns_error_check(ip1_session,ips,crash_dict)
+							
         if '-c' in sys.argv:
 	    for ips in ip:
 		if len(ips)>0:
@@ -285,7 +302,7 @@ def main():
 	    		cmd=exec_cmd(ip1_session,"sh license | grep NO")
             		logger.info(cmd)	
             		cmd=exec_cmd(ip1_session,"sh version")
-            		logger.infox(cmd)
+            		logger.info(cmd)
 	    		var=ns_error_check(ip1_session,ip1)
             		if var==0:
                 		logger.info("Done checking errors on "+ips)
@@ -325,10 +342,10 @@ def main():
             else:
                 logger.info("FAILED\nPlease correct the testbed")	
 	if '-ap' in sys.argv:
-	    cmd=os.system("partition_maker 10.106.81.170 1")
-	    logger.info(cmd)
-	    time.sleep(5)  
-	    session=ssh_login('10.106.81.170','nsroot','nsroot','>')
+	    #cmd=os.system("partition_maker 10.106.81.170 1")
+	    #logger.info(cmd)
+	    #time.sleep(5)  
+	    session=ssh_login('10.106.81.171','nsroot','nsroot','>')
 	    cmd=exec_cmd(session,"add vlan 95 -sharing enabled")
 	    logger.info(cmd)	
 	    cmd=exec_cmd(session,"bind vlan 95 -ifnum 1/2 1/3")
@@ -349,7 +366,7 @@ def main():
 	    logger.info(cmd)
 	    cmd=exec_cmd(session,"savec")
 	    logger.info(cmd)
-            cmd=exec_cmd(session,"ping -c 4 10.102.1.98")
+            cmd=exec_cmd(session,"ping -c 4 10.102.1.97")
 	    logger.info(cmd)
 
    # except:
